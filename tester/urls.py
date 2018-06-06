6a197bcd71dd9ad8b3f7d4bbb4bfe268f022a0fb
matsapura98 <matsapura98@gmail.com>
2018-06-06 19:45:15 +0300

    HTML, CSS, JS, views.py, urls.py, urls.py

--- tester/urls.py
+++ tester/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('history/', views.history, name='history')
 ]
--- tester/views.py
+++ tester/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def test(request):
     return render(request, 'tester/test.html')
+def history(request):
+    return render(request, 'tester/history.html')
+def signin(request):
+    return render(request, 'tester/signin.html')
+def signup(request):
+    return render(request, 'tester/signup.html')
+
